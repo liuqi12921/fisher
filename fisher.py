97966113f50a06e6befe19623781d2636da5fbd0
liuqi <liuqi12921@gmail.com>
2018-08-12 21:26:33 +0800

    添加yushu_book模块用于封装请求API功能

--- fisher.py
+++ fisher.py
@@ -1,5 +1,10 @@
+#import json
 from flask import Flask
+from flask import jsonify
 from helper import is_isbn_or_key
+from yushu_book import YuShuBook
+
+
 
 app = Flask(__name__)
 
@@ -10,7 +15,17 @@ def search(q, page):
     page
     """
     isbn_or_key = is_isbn_or_key(q)
-    pass
+
+    if isbn_or_key == 'isbn':
+        result = YuShuBook.search_by_isbn(q)
+    else:
+        result = YuShuBook.search_by_keyword(q)
+
+    # dict序列化 使用python自带json模块
+    # return json.dumps(result), 200, {'content-type':'application/json'}
+
+    # dict序列化 使用flask的jsonify
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
