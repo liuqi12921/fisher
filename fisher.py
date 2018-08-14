5c292d87fc2dc5dbc211682ca49a3f7b816a621e
liuqi <liuqi12921@gmail.com>
2018-08-14 16:49:24 +0800

    将fisher.py中的app初始化代码分离到app包中

--- fisher.py
+++ fisher.py
@@ -1,13 +1,8 @@
-from flask import Flask
 
-app = Flask(__name__)
-app.config.from_object('config')
+from app import create_app
 
-print('id为' + str(id(app)) + '的app实例化')
-
-from app.web import book
+app = create_app()
 
 if __name__ == "__main__":
     # 进行调试的时候需要将debug设置为False以阻止Flask的调试
     app.run(host='0.0.0.0', debug=False, port=5000)
-
